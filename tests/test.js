fe6f85bce2967fec4dc375ed49f85fc02eea9417
amaapp333 <118077998+amaapp333@users.noreply.github.com>
2022-12-25 05:22:14 +0200

    Create test.js

---
+++ tests/test.js
@@ -0,0 +1,8 @@
+const isOdd = require('../src/odd.js')
+QUnit.test("is 1 odd", function( assert ) {
+  assert.ok(isOdd(1), 'One is an odd number');
+});
+
+QUnit.test("is 2 odd", function( assert ) {
+  assert.notOk(isOdd(2), 'two is an even number');
+});
